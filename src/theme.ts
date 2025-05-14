05bd2d2b53357a3558a864360c87cee3a17a3a57
fatherwolf <engleau13@gmail.com>
2025-05-13 20:38:15 -0400

    added colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,38 @@
+// src/theme.ts
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  palette: { mode: 'light' },
-  typography: { fontFamily: 'Roboto, sans-serif' },
+  palette: {
+    primary: {
+      light: '#E5383B',
+      main: '#BA181B',
+      dark: '#660708',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#161A1D',
+      contrastText: '#FFFFFF',
+    },
+    background: {
+      default: '#F5F3F4',
+      paper: '#FFFFFF',
+    },
+    text: {
+      primary: '#0B090A',
+      secondary: '#161A1D',
+    },
+    error: {
+      main: '#A4161A',
+    },
+    info: {
+      main: '#B1A7A6',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+    h1: { fontWeight: 700 },
+    h2: { fontWeight: 600 },
+  },
 });
 
 export default theme;
